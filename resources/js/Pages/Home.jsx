f6a0e64a8593cad26469effff9ea3bba6bba5521
ehroy <anjaykoko69@gmail.com>
2024-06-18 12:44:07 +0700

    remake

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,5 +1,33 @@
-import React from "react";
-import Header from "../component/Header";
+import { React } from "react";
+import Logo from "../atribute/logo.png";
 export default function Home() {
-    return <Header />;
+    return (
+        <headers className="flex justify-between bg-gray-50">
+            <nav className="p-2">
+                <div className="text-2xl cursor-pointer" id="logo">
+                    <span className="flex items-center font-semibold text-xl">
+                        <img
+                            src={Logo}
+                            className="rounded-full size-12 items-start p-1"
+                        />
+                        Decoration Jepara
+                        <ul className="flex flex-row justify-between">
+                            <li>
+                                <a href="">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="">Dashboard</a>
+                            </li>
+                        </ul>
+                    </span>
+                </div>
+            </nav>
+        </headers>
+    );
 }
--- resources/js/component/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Link({ children }) {
-    return (
-        <a href="#" className="hover:text-blue-200 w-28">
-            {children}
-        </a>
-    );
-}
