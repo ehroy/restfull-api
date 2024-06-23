7d94024ca10e29cbbdf0301213e516c9092b2275
ehroy <ahmadroy092@gmail.com>
2024-06-23 14:19:57 +0700

    update-page

--- resources/js/component/ListProduct.jsx
+++ resources/js/component/ListProduct.jsx
@@ -1,50 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import Products from "../atribute/logo.png";
 const Product = [
-    { image: Products, name: "Kursi minimalis", price: "Rp. 20.000.000" },
-    { image: Products, name: "Kursi minimalis", price: "Rp. 20.000.000" },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
     {
-        image: "https://placehold.co/400x600?text=3",
+        image: Products,
         name: "Kursi minimalis",
         price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
+    },
+    {
+        image: Products,
+        name: "Kursi minimalis",
+        price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
+    },
+    {
+        image: Products,
+        name: "Kursi minimalis",
+        price: "Rp. 20.000.000",
+        lokasi: "Jepara",
+        Terjual: 0,
     },
 ];
+const Count = () => {
+    const [add, setAdd] = useState(0);
+    // if()
+};
 export default function ListProduct({ children }) {
     return (
         <>
-            <div className="m-5 flex flex-col gap-24">
-                <div className="grid grid-cols-4 gap-4">
+            <div className="card m-5 flex flex-col gap-24 ">
+                <div className="grid grid-cols-4 gap-4 ">
                     {Product.map((images, index) => (
-                        <img
-                            className="hover:will-change-transform"
-                            src={images.image}
-                            alt={index}
-                        ></img>
+                        <>
+                            <div className="shadow-xl rounded-xl">
+                                <img
+                                    className="hover:will-change-transform p-2"
+                                    src={images.image}
+                                    alt={index}
+                                ></img>
+                                <div className=" p-4">
+                                    <h1 className="font-serif">
+                                        {images.name}
+                                    </h1>
+                                    <p>{images.price}</p>
+                                    <p>{images.lokasi}</p>
+                                    <div className="flex">
+                                        <p>{images.Terjual}</p>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="size-5"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                                            />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
                     ))}
                 </div>
             </div>
--- resources/js/component/MainText.jsx
+++ resources/js/component/MainText.jsx
@@ -13,16 +13,38 @@ export default function MainText() {
     return (
         <div>
             <div className="m-5  flex-col gap-10">
-                <div className="container grid grid-cols-3 gap-4 mx-auto ">
-                    <ul className="flex px-10 py-10 ">
-                        <li className="items-center justify-center absolute">
+                <div className="p-5 grid grid-cols-1 gap-4 mx-auto md:grid-cols-4">
+                    <ul className="font-serif hover:animate-bounce">
+                        <li className="py-12 px-12 mx-auto gap-4 shadow-xl rounded-lg justify-items-center">
+                            <a href="" className="relative">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-8"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                                    />
+                                </svg>
+                            </a>
+
+                            <span className="">Pencarian Mudah</span>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="py-12 px-12 mx-auto gap-4 shadow-xl rounded-lg">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
-                                className="size-6"
+                                className="size-8 relative"
                             >
                                 <path
                                     strokeLinecap="round"
@@ -30,57 +52,60 @@ export default function MainText() {
                                     d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                                 />
                             </svg>
-                            cari product impianmu untuk mengisi ruang rumahmu
-                        </li>
 
-                        <li className="items-center justify-center ">
+                            <span className="">Pemesanan Mudah</span>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="py-12 px-12 mx-auto gap-4 shadow-xl rounded-lg justify-center items-center">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
-                                className="size-6"
+                                className="size-8 relative "
                             >
                                 <path
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
-                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                                 />
                             </svg>
-                            <div className="">
-                                <span>PELI</span>
-                            </div>
-                        </li>
 
-                        <li className="items-center justify-center ">
+                            <span className="">Pengiriman cepat dan aman</span>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="py-12 px-12 mx-auto gap-4 shadow-xl rounded-lg">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
-                                className="size-6"
+                                className="size-8 relative"
                             >
                                 <path
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
-                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                                 />
                             </svg>
-                            <div className="">
-                                <span>PELI</span>
-                            </div>
+
+                            <span className="">Harga dan kualitas bagus</span>
                         </li>
                     </ul>
                 </div>
+                <div className="flex justify-center text-3xl font-bold p-2 mx-auto">
+                    <h1 className="text-gray-600 font-serif">WELCOME</h1>
+                </div>
                 <div className="sm:col-span-12 lg:col-span-8 text-justify">
-                    <h1 className="text-gray-600">WELCOME</h1>
                     <span className="sm:text-base lg:text-lg text-center">
                         Selamat datang di web katalog Kami{" "}
                         <span className="font-extrabold">
                             Decoration Jepara
-                        </span>{" "}
+                        </span>
                         adalah produsen furniture mebel asli Jepara. Dengan
                         keutamaan bahan kayu mahoni maupun jati bebas custom,
                         kami menyediakan berbagai mebel seperti kursi tamu kayu
